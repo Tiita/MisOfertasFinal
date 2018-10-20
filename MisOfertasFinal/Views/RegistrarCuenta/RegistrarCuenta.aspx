090e6a65331257f8dfb0599a8d2e3549684de3b9
Tiita <44036325+Tiita@users.noreply.github.com>
2018-10-19 22:36:22 -0300

    Actualización versión web final

---
+++ MisOfertasFinal/Views/RegistrarCuenta/RegistrarCuenta.aspx
@@ -0,0 +1,105 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="RegistrarCuenta.aspx.cs" Inherits="MisOfertasFinal.Views.RegistrarCuenta.RegistrarCuenta" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Crear cuenta cliente</title>
+    <!-- Bootstrap core CSS-->
+    <link href="../../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+    <!-- Custom fonts for this template-->
+    <link href="../../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <!-- Custom styles for this template-->
+    <link href="../../css/sb-admin.css" rel="stylesheet" />
+    <!-- Bootstrap core JavaScript-->
+    <script src="../../vendor/jquery/jquery.min.js"></script>
+    <script src="../../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- Core plugin JavaScript-->
+    <script src="../../vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <link href="../../css/sweetalert.css" rel="stylesheet" />
+    <script src="../../js/sweetalert.min.js" type="text/javascript"></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="mx-auto mt-5">
+            <%--PRIMER GRUPO DE CONTROLES--%>
+
+            <div class="card">
+                <div class="card-header">
+                    Bienvenido Mis Ofertas
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Crear cuenta cliente</h5>                    
+                        <div class="form-group">
+                            <div class="form-row">
+                                <div class="col-md-2">
+                                    <p class="card-text">
+                                    <asp:Label runat="server" ID="lblNombreCliente" Text="Nombre:"></asp:Label>
+                                    <asp:TextBox runat="server" ID="txtNombreCliente" CssClass="form-control"></asp:TextBox>
+                                    </p>
+                                </div>
+                                <div class="col-md-2">
+                                    <p class="card-text">
+                                        <asp:Label runat="server" ID="lblApePaterno" Text="Apellido Paterno:"></asp:Label>
+                                        <asp:TextBox runat="server" ID="txtApePaterno" CssClass="form-control"></asp:TextBox>
+                                    </p>                                    
+                                </div>
+                                <div class="col-md-2">
+                                    <p class="card-text">
+                                        <asp:Label runat="server" ID="lblApeMaterno" Text="Apellido Materno:"></asp:Label>
+                                        <asp:TextBox runat="server" ID="txtApeMaterno" CssClass="form-control"></asp:TextBox>
+                                    </p>                                    
+                                </div>
+                                <div class="col-md-2">
+                                    <p class="card-text">
+                                        <asp:Label runat="server" ID="lblCorreo" Text="Email:"></asp:Label>
+                                        <asp:TextBox runat="server" ID="txtCorreo" CssClass="form-control" placeholder="name@example.com"></asp:TextBox>
+                                    </p>                                    
+                                </div>
+                                <div class="col-md-2">
+                                    <p class="card-text">
+                                        <asp:Label runat="server" ID="lblRut" Text="Rut cliente:"></asp:Label>
+                                        <asp:TextBox runat="server" ID="txtRut" CssClass="form-control"></asp:TextBox>
+                                    </p>                                    
+                                </div>
+                                <div class="col-md-2">
+                                    <p class="card-text">
+                                        <asp:Label runat="server" ID="lblDireccion" Text="Dirección:"></asp:Label>
+                                        <asp:TextBox runat="server" ID="txtDireccion" CssClass="form-control"></asp:TextBox>
+                                    </p>                                    
+                                </div>
+                                <div class="col-md-2">
+                                    <p class="card-text">
+                                        <asp:Label runat="server" ID="lblFono" Text="Teléfono:"></asp:Label>
+                                        <asp:TextBox runat="server" ID="txtFono" CssClass="form-control"></asp:TextBox>
+                                    </p>                                    
+                                </div>
+                                <div class="col-md-2">
+                                    <p class="card-text">
+                                        <asp:Label runat="server" ID="lblPassword" Text="Password:"></asp:Label>
+                                        <asp:TextBox runat="server" ID="txtPassword" CssClass="form-control" placeholder="Password" TextMode="Password"></asp:TextBox>
+                                    </p>                                    
+                                </div>                                
+                            </div>
+                        </div>
+                    </p>
+                     <%--SEGUNDO GRUPO DE CONTROLES--%>
+                    <div class="form-group">
+                        <div class="form-row">
+                            <p class="card-text">
+                                <asp:Button runat="server" ID="btnGuardar" Text="Guardar" CssClass="btn btn-success" />                                
+                            </p>                            
+                        </div>
+                    </div>                            
+                </div>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ MisOfertasFinal/Views/RegistrarCuenta/RegistrarCuenta.aspx.designer.cs
@@ -0,0 +1,177 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace MisOfertasFinal.Views.RegistrarCuenta {
+    
+    
+    public partial class RegistrarCuenta {
+        
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Control lblNombreCliente.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblNombreCliente;
+        
+        /// <summary>
+        /// Control txtNombreCliente.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombreCliente;
+        
+        /// <summary>
+        /// Control lblApePaterno.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblApePaterno;
+        
+        /// <summary>
+        /// Control txtApePaterno.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtApePaterno;
+        
+        /// <summary>
+        /// Control lblApeMaterno.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblApeMaterno;
+        
+        /// <summary>
+        /// Control txtApeMaterno.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtApeMaterno;
+        
+        /// <summary>
+        /// Control lblCorreo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblCorreo;
+        
+        /// <summary>
+        /// Control txtCorreo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCorreo;
+        
+        /// <summary>
+        /// Control lblRut.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblRut;
+        
+        /// <summary>
+        /// Control txtRut.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtRut;
+        
+        /// <summary>
+        /// Control lblDireccion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblDireccion;
+        
+        /// <summary>
+        /// Control txtDireccion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDireccion;
+        
+        /// <summary>
+        /// Control lblFono.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblFono;
+        
+        /// <summary>
+        /// Control txtFono.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtFono;
+        
+        /// <summary>
+        /// Control lblPassword.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblPassword;
+        
+        /// <summary>
+        /// Control txtPassword.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPassword;
+        
+        /// <summary>
+        /// Control btnGuardar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnGuardar;
+    }
+}
